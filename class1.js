3855b63a31575d993aa3eb77d53d5bf538be05f0
SoumiliD <92991995+SoumiliD@users.noreply.github.com>
2023-06-21 00:43:39 +0530

    added data types and operators in js

--- class1.js
+++ class1.js
@@ -91,3 +91,48 @@
 // const number = 50
 
 
+// There are different types of JavaScript operators:
+// Arithmetic Operators (+, -, /, *, %, **, ++, --)
+// Assignment Operators (=, +=, -=, *=, /=, **=)
+// Comparison Operators (<, > , <=, >=, ==, ===, !=, !==)
+// String Operators (concat +)
+// Logical Operators (&&, ||, !)
+// Bitwise Operators (&, |, ~, ^, <<, >>)
+// Ternary Operators (x : y ? z)
+// Type Operators (typeof(), instanceof())
+
+
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+
+// Numbers:
+// let length = 16;
+// let weight = 7.5;
+
+// Strings:
+// let color = "Yellow";
+
+// Booleans
+// let x = true;
+// let y = false;
+
+// Object:
+// const person = {
+    // firstName:"John", 
+    // lastName:"Doe"
+// };
+
+// Array object:
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+// const date = new Date("2022-03-25");
+
